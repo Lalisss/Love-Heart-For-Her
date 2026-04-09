a28db9f56559f851779cabdef7ffe47060246593
Lalisss <109070979+Lalisss@users.noreply.github.com>
2026-04-10 01:15:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
-camera.position.z = 10;
+camera.position.z = 25;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
